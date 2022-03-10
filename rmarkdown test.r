1a480489c367fd05ed6cc5c50c0cdc207db0ac05
Xiaolin Wang <xwangiub@gmail.com>
2022-03-09 21:12:03 -0600

    .r format

---
+++ rmarkdown test.r
@@ -0,0 +1,12 @@
+#`---
+#`title: "rmarkdown test"
+#`author: "XW"
+#`date: '2022-03-10'
+#`output: 
+#`  html_document: 
+#`    keep_md: yes
+#`---
+#`r, eval=TRUE
+## insert here 
+hist(rnorm(100, mean=0, sd=1))
+#````
